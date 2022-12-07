d4c33554cf8450c0cfb92ee38a82fd61456d46a8
Mike Burton <mikeburtoncodes@gmail.com>
2022-12-07 15:14:08 -0500

    y2022d06 (#3)

---
+++ golang/y2022/d06/solution.go
@@ -0,0 +1,89 @@
+package d06
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/Mike-Burton/advent-of-code/hashset"
+	"github.com/Mike-Burton/advent-of-code/helpers"
+)
+
+const (
+	PARTONE = 4
+	PARTTWO = 14
+)
+
+// PartOne solves the first problem of day 6 of Advent of Code 2022.
+func PartOne(r io.Reader, w io.Writer) error {
+
+	chars, err := signalFromReader(r)
+	if err != nil {
+		return fmt.Errorf("could not read input: %w", err)
+	}
+
+	i := 3
+	for ; i < len(chars); i++ {
+		hs := hashset.New(chars[i], chars[i-1], chars[i-2], chars[i-3])
+		if hs.Size() == 4 {
+			break
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "%d", i+1)
+	if err != nil {
+		return fmt.Errorf("could not write answer: %w", err)
+	}
+
+	return nil
+}
+
+// PartTwo solves the second problem of day 6 of Advent of Code 2022.
+func PartTwo(r io.Reader, w io.Writer) error {
+	chars, err := signalFromReader(r)
+	if err != nil {
+		return fmt.Errorf("could not read input: %w", err)
+	}
+
+	i := 13
+	for ; i < len(chars); i++ {
+		hs := hashset.New(
+			chars[i],
+			chars[i-1],
+			chars[i-2],
+			chars[i-3],
+			chars[i-4],
+			chars[i-5],
+			chars[i-6],
+			chars[i-7],
+			chars[i-8],
+			chars[i-9],
+			chars[i-10],
+			chars[i-11],
+			chars[i-12],
+			chars[i-13],
+		)
+
+		if hs.Size() == 14 {
+			break
+		}
+	}
+	_, err = fmt.Fprintf(w, "%d", i+1)
+	if err != nil {
+		return fmt.Errorf("could not write answer: %w", err)
+	}
+
+	return nil
+}
+
+func signalFromReader(r io.Reader) (chars []rune, err error) {
+	lines, err := helpers.LinesFromReader(r)
+	if err != nil {
+		return nil, fmt.Errorf("could not read input: %w", err)
+	}
+
+	for _, r := range lines[0] {
+		chars = append(chars, r)
+	}
+
+	return
+}

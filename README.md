9a9541309bec88bb53592e2a41925c3b514d783c
Mike Burton <mike.burton@hqo.co>
2022-12-07 15:13:36 -0500

    y2022d06

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # advent-of-code
 
 ## 2021
+* [d01 (python)](./python/y2021/day-01.py)
+* [d02 (python)](./python/y2021/day-02.py)
+* [d04 (python)](./python/y2021/day-04.py)
+* [d07 (python)](./python/y2021/day-07.py)
+* [d14 (python)](./python/y2021/day-14.py)
+
+## 2022
+* [d01 (golang)](./golang/y2022/d01/solution.go)
+* [d02 (golang)](./golang/y2022/d02/solution.go)
+* [d03 (golang)](./golang/y2022/d03/solution.go)
+* [d04 (golang)](./golang/y2022/d04/solution.go)
+* [d05 (golang)](./golang/y2022/d05/solution.go)
+* [d06 (golang)](./golang/y2022/d06/solution.go)
---
+++ golang/y2022/d06/solution_test.go
@@ -0,0 +1,58 @@
+package d06
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/Mike-Burton/advent-of-code/helpers"
+)
+
+func ExamplePartOne() {
+	file, err := os.Open("testdata/input.txt")
+	if err != nil {
+		log.Fatalf("could not open input file: %v", err)
+	}
+	defer file.Close()
+
+	if err := PartOne(file, os.Stdout); err != nil {
+		log.Fatalf("could not solve: %v", err)
+	}
+	// Output: 1343
+}
+
+func ExamplePartTwo() {
+	file, err := os.Open("testdata/input.txt")
+	if err != nil {
+		log.Fatalf("could open input file: %v", err)
+	}
+	defer file.Close()
+
+	if err := PartTwo(file, os.Stdout); err != nil {
+		log.Fatalf("could not solve: %v", err)
+	}
+	// Output: 2193
+}
+
+func Benchmark(b *testing.B) {
+	testCases := map[string]struct {
+		solution  helpers.Solution
+		inputFile string
+	}{
+		"PartOne": {
+			solution:  helpers.SolutionFunc(PartOne),
+			inputFile: "testdata/input.txt",
+		},
+
+		"PartTwo": {
+			solution:  helpers.SolutionFunc(PartTwo),
+			inputFile: "testdata/input.txt",
+		},
+	}
+
+	for name, test := range testCases {
+		b.Run(name, func(b *testing.B) {
+			helpers.BenchmarkSolution(b, test.solution, test.inputFile)
+		})
+	}
+}
